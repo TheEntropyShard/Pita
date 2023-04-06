a4b094d86d9ef1698a5c4d8121efe65c4a86a801
TheEntropyShard <theentropyshard@gmail.com>
2023-04-06 20:20:07 +0300

    Создан MessageDialog.java

---
+++ src/main/java/me/theentropyshard/pita/view/MessageDialog.java
@@ -0,0 +1,47 @@
+/*      Pita. A simple desktop client for NetSchool by irTech
+ *      Copyright (C) 2022-2023 TheEntropyShard
+ *
+ *      This program is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package me.theentropyshard.pita.view;
+
+import me.theentropyshard.pita.view.component.GradientLabel;
+import me.theentropyshard.pita.view.mail.InfoPanel;
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MessageDialog extends JDialog {
+    public MessageDialog(String title, String message) {
+        InfoPanel infoPanel = new InfoPanel();
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.WHITE);
+        panel.setLayout(new MigLayout("fillx, flowy", "[fill]"));
+        panel.add(infoPanel);
+
+        GradientLabel label = new GradientLabel(message, UIConstants.DARK_GREEN, UIConstants.LIGHT_GREEN);
+        label.setFont(new Font("JetBrains Mono", Font.BOLD, 16));
+        infoPanel.addDataPanel(label);
+
+        this.setTitle(title);
+        this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        this.add(panel, BorderLayout.CENTER);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+}

e2961dab63a5adae790f85e7b8da9daeb2562b30
TheEntropyShard <theentropyshard@gmail.com>
2023-04-07 18:30:13 +0300

    SchoolInfoPanel.java: удален вложенный класс

--- src/main/java/me/theentropyshard/pita/view/SchoolInfoPanel.java
+++ src/main/java/me/theentropyshard/pita/view/SchoolInfoPanel.java
@@ -26,8 +26,6 @@ import net.miginfocom.swing.MigLayout;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
 import java.io.IOException;
 
 public class SchoolInfoPanel extends JPanel {
@@ -119,57 +117,6 @@ public class SchoolInfoPanel extends JPanel {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-    }
-
-    private static class InfoPanel extends JPanel {
-        private final JPanel internalInfoPanel;
-
-        public InfoPanel() {
-            this.setLayout(new MigLayout("nogrid, fillx", "[]", ""));
-            this.setOpaque(false);
-            this.addComponentListener(new ComponentAdapter() {
-                @Override
-                public void componentResized(ComponentEvent e) {
-                    revalidate();
-                }
-            });
-            this.setBorder(BorderFactory.createCompoundBorder(this.getBorder(), BorderFactory.createEmptyBorder(5, 5, 5, 5)));
-
-            this.internalInfoPanel = new JPanel(new MigLayout("fillx, flowy", "[fill]")) {
-                {
-                    this.setOpaque(false);
-                    this.addComponentListener(new ComponentAdapter() {
-                        @Override
-                        public void componentResized(ComponentEvent e) {
-                            revalidate();
-                        }
-                    });
-                }
-
-                @Override
-                protected void paintComponent(Graphics g) {
-                    Graphics2D g2 = (Graphics2D) g;
-                    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                    g2.setColor(Color.WHITE);
-                    g2.fillRoundRect(0, 0, this.getWidth(), this.getHeight(), UIConstants.ARC_WIDTH, UIConstants.ARC_HEIGHT);
-                    super.paintComponent(g2);
-                }
-            };
-            this.add(this.internalInfoPanel, "grow");
-        }
-
-        @Override
-        protected void paintComponent(Graphics g) {
-            Graphics2D g2 = (Graphics2D) g;
-            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            g2.setColor(UIConstants.NEAR_WHITE2);
-            g2.fillRoundRect(0, 0, this.getWidth(), this.getHeight(), UIConstants.ARC_WIDTH, UIConstants.ARC_HEIGHT);
-            super.paintComponent(g2);
-        }
-
-        public void addDataPanel(DataElementPanel dataPanel) {
-            this.internalInfoPanel.add(dataPanel);
         }
     }
 

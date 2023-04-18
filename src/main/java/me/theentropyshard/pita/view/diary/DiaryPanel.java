c562fd22382eaafc99287bc8f0fe65e7fc9532d6
TheEntropyShard <theentropyshard@gmail.com>
2023-04-18 21:18:19 +0300

    DiaryPanel.java: теперь на днях отображается дата

--- src/main/java/me/theentropyshard/pita/view/diary/DiaryPanel.java
+++ src/main/java/me/theentropyshard/pita/view/diary/DiaryPanel.java
@@ -34,6 +34,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.HierarchyEvent;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
@@ -110,36 +111,35 @@ public class DiaryPanel extends JPanel {
     }
 
     public void loadData() {
-        if(true) {
-            try {
-                //Diary diary = NetSchoolAPI.I.getDiary("2023-02-13", "2023-02-19");
-                Diary diary = NetSchoolAPI.I.getDiary("2023-04-10", "2023-04-15");
-                for(int dayNum = 0; dayNum < diary.weekDays.length; dayNum++) {
-                    Day diaryDay = diary.weekDays[dayNum];
-                    DiaryDay dayElement = this.days[dayNum];
-                    for(int lessonNum = 0; lessonNum < diaryDay.lessons.length; lessonNum++) {
-                        Lesson lesson = diaryDay.lessons[lessonNum];
-                        DiaryLesson diaryLesson = new DiaryLesson(0, "", " ");
-                        try {
-                            diaryLesson = dayElement.lessons.get(lesson.number - 1);
-                        } catch (IndexOutOfBoundsException e) {
-                            e.printStackTrace();
-                        }
-                        diaryLesson.lessonNameLabel.setText(diaryLesson.lessonNameLabel.getText() + lesson.subjectName);
-                        if(lesson.assignments != null) {
-                            for(Assignment assignment : lesson.assignments) {
-                                if(assignment.typeId == DiaryPanel.TYPE_ID_HOMEWORK) {
-                                    diaryLesson.homeworkLabel.setText(assignment.assignmentName);
-                                } else if(assignment.mark != null) {
-                                    diaryLesson.addMark(String.valueOf(assignment.mark.mark));
-                                }
+        try {
+            //Diary diary = NetSchoolAPI.I.getDiary("2023-02-13", "2023-02-19");
+            Diary diary = NetSchoolAPI.I.getDiary("2023-04-10", "2023-04-15");
+            for(int dayNum = 0; dayNum < diary.weekDays.length; dayNum++) {
+                Day diaryDay = diary.weekDays[dayNum];
+                DiaryDay dayElement = this.days[dayNum];
+                dayElement.setDate(LocalDateTime.parse(diaryDay.date).format(DiaryPanel.DAY_DATE_FORMATTER));
+                for(int lessonNum = 0; lessonNum < diaryDay.lessons.length; lessonNum++) {
+                    Lesson lesson = diaryDay.lessons[lessonNum];
+                    DiaryLesson diaryLesson = new DiaryLesson(0, "", " ");
+                    try {
+                        diaryLesson = dayElement.lessons.get(lesson.number - 1);
+                    } catch (IndexOutOfBoundsException e) {
+                        e.printStackTrace();
+                    }
+                    diaryLesson.lessonNameLabel.setText(diaryLesson.lessonNameLabel.getText() + lesson.subjectName);
+                    if(lesson.assignments != null) {
+                        for(Assignment assignment : lesson.assignments) {
+                            if(assignment.typeId == DiaryPanel.TYPE_ID_HOMEWORK) {
+                                diaryLesson.homeworkLabel.setText(assignment.assignmentName);
+                            } else if(assignment.mark != null) {
+                                diaryLesson.addMark(String.valueOf(assignment.mark.mark));
                             }
                         }
                     }
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -166,6 +166,7 @@ public class DiaryPanel extends JPanel {
 
     private static class DiaryDay extends BorderPanel {
         private final List<DiaryLesson> lessons;
+        private GradientLabel dateLabel;
 
         public DiaryDay() {
             this.lessons = new ArrayList<>();
@@ -177,6 +178,11 @@ public class DiaryPanel extends JPanel {
         }
 
         public void initLessons() {
+            this.dateLabel = new GradientLabel();
+            this.dateLabel.setFont(new Font("JetBrains Mono", Font.BOLD, 14));
+            this.addComponent(this.dateLabel, "center, span, wrap");
+            this.addComponent(new GradientLabel(" "), "center, span, wrap");
+
             this.addHeader(new DiaryLesson(0, "Урок", "Домашнее задание") {{
                 this.addMark("Оценка");
             }});
@@ -184,6 +190,11 @@ public class DiaryPanel extends JPanel {
             for(int i = 0; i < 8; i++) {
                 this.add(new DiaryLesson(i + 1, "", " "));
             }
+        }
+
+        public void setDate(String date) {
+            this.dateLabel.setText(date);
+            this.repaint();
         }
 
         public void addHeader(DiaryLesson lesson) {

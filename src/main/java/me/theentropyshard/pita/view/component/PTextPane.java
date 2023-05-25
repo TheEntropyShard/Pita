725498ef2948a204476268d466b5c613043c0a24
TheEntropyShard <theentropyshard@gmail.com>
2023-05-25 16:52:35 +0300

    PTextPane.java: добавлен antialiasing для текста

--- src/main/java/me/theentropyshard/pita/view/component/PTextPane.java
+++ src/main/java/me/theentropyshard/pita/view/component/PTextPane.java
@@ -52,6 +52,7 @@ public class PTextPane extends JTextPane {
     protected void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         g2.setColor(Color.WHITE);
         g2.fillRoundRect(0, 0, this.getWidth(), this.getHeight(), 7, 7);
         super.paintComponent(g2);
